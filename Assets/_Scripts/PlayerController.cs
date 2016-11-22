2a4cc07228e0352642a918722a180223b6211182
PailWind <palu83xoo@gmail.com>
2016-11-22 16:59:20 -0500

    second commit

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int Speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * Speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+			{
+				other.gameObject.SetActive(false);
+				count = count + 1;
+				SetCountText ();
+			}
+
+		if (count >= 6)
+		{
+			winText.text = "You Win!";
+		}
+	}
+	void SetCountText()
+	{
+		countText.text = "Score: " + count.ToString ();
+	}
+}
